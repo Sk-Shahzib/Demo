73fdb9015810e3891123a0d7ba99fec680cbd9df
Sk-Shahzib <shaikhshahzib19@gmail.com>
2024-07-06 15:50:46 +0530

    Update file

---
+++ Newflinn2/ddf.java
@@ -0,0 +1,10 @@
+package NewFolder.Newflinn2;
+
+public class ddf {
+    public static void main(String[] args) {
+        int n = 10;
+        for (int i = 1; i < n; i++) {
+            System.out.println(i);
+        }
+    }
+}
